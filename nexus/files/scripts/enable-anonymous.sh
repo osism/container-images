d74171bba687b30d4bc2ffa7aca45741e917d341
Christian Berendt <berendt@osism.tech>
2021-12-17 13:06:12 +0100

    nexus: add wait script
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- nexus/files/scripts/enable-anonymous.sh
+++ nexus/files/scripts/enable-anonymous.sh
@@ -2,8 +2,13 @@
 
 NEXUS_HOST=${NEXUS_HOST:-localhost}
 NEXUS_PORT=${NEXUS_PORT:-8081}
+NEXUS_PATH=${NEXUS_PATH:-}
 NEXUS_USERNAME=${NEXUS_USERNAME:-admin}
 NEXUS_PASSWORD=${NEXUS_PASSWORD:-password}
 
+WAIT_TIMEOUT=120
+WAIT_HOSTS=${NEXUS_HOST}:${NEXUS_PORT}
+/usr/local/bin/wait
+
 curl -v -X POST -u "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" \
-    --header "Content-Type: text/plain" "http://${NEXUS_HOST}:${NEXUS_PORT}/service/rest/v1/script/anonymous/run" -d true
+    --header "Content-Type: text/plain" "http://${NEXUS_HOST}:${NEXUS_PORT}${NEXUS_PATH}/service/rest/v1/script/anonymous/run" -d true
--- nexus/files/scripts/run.sh
+++ nexus/files/scripts/run.sh
@@ -2,11 +2,16 @@
 
 NEXUS_HOST=${NEXUS_HOST:-localhost}
 NEXUS_PORT=${NEXUS_PORT:-8081}
+NEXUS_PATH=${NEXUS_PATH:-}
 NEXUS_USERNAME=${NEXUS_USERNAME:-admin}
 NEXUS_PASSWORD=${NEXUS_PASSWORD:-password}
 
 NAME=$1
 
+WAIT_TIMEOUT=120
+WAIT_HOSTS=${NEXUS_HOST}:${NEXUS_PORT}
+/usr/local/bin/wait
+
 curl -v -X POST -u "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" \
     --header "Content-Type: text/plain" \
-    "http://${NEXUS_HOST}:${NEXUS_PORT}/service/rest/v1/script/${NAME}/run"
+    "http://${NEXUS_HOST}:${NEXUS_PORT}${NEXUS_PATH}/service/rest/v1/script/${NAME}/run"
