0a9e42d56dd8f8dfbf42e694f472526ae3ff83e3
Christian Berendt <berendt@osism.tech>
2021-12-28 21:08:54 +0100

    nexus: fix healthcheck (#13)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- nexus/files/scripts/run.sh
+++ nexus/files/scripts/run.sh
@@ -8,9 +8,14 @@ NEXUS_PASSWORD=${NEXUS_PASSWORD:-password}
 
 NAME=$1
 
-WAIT_TIMEOUT=120
-WAIT_HOSTS=${NEXUS_HOST}:${NEXUS_PORT}
-/usr/local/bin/wait
+# https://gist.github.com/rgl/f90ff293d56dbb0a1e0f7e7e89a81f42#gistcomment-3773747
+HEALTH_CHECK_URL=http://${NEXUS_HOST}:${NEXUS_PORT}${NEXUS_PATH}/service/rest/v1/status STATUS=200 timeout --foreground -s TERM 120 bash -c \
+    'while [[ ${STATUS_RECEIVED} != ${STATUS} ]];\
+        do STATUS_RECEIVED=$(curl -s -o /dev/null -L -w "%{http_code}" ${HEALTH_CHECK_URL}) && \
+        echo "received status: $STATUS_RECEIVED" && \
+        sleep 1;\
+    done;
+    echo success with status: $STATUS_RECEIVED'
 
 curl -v -X POST -u "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" \
     --header "Content-Type: text/plain" \
