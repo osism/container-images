0077baa40007b4051b336d2ac6755611debc94d9
Christian Berendt <berendt@osism.tech>
2022-03-04 17:32:02 +0100

    ara-server: use unit
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- ara-server/files/run.sh
+++ ara-server/files/run.sh
@@ -4,10 +4,6 @@
 #
 # ARA_API_PASSWORD
 # ARA_API_USERNAME
-# ARA_HOST
-# ARA_PORT
-# ARA_WORKERS
-# ARA_WORKER_CLASS
 #
 # https://ara.readthedocs.io/en/latest/api-configuration.html#overview
 
@@ -15,19 +11,27 @@
 
 ARA_API_PASSWORD=${ARA_API_PASSWORD:-password}
 ARA_API_USERNAME=${ARA_API_USERNAME:-ara}
-ARA_HOST=${ARA_HOST:-0.0.0.0}
-ARA_PORT=${ARA_PORT:-8000}
-ARA_WORKERS=${ARA_WORKERS:-5}
-ARA_WORKER_CLASS=${ARA_WORKER_CLASS:-sync}
+
+export ARA_BASE_DIR=/var/lib/ara-server
+
+cp /var/lib/ara-server/ara-server.yml /var/lib/ara-server/settings.yaml
+chown unit:root /var/lib/ara-server/settings.yaml
 
 until ara-manage migrate; do
     echo "database migration failed, trying again in 10 seconds"
     sleep 10
 done
 
+ara-manage collectstatic
+
 result=$(echo "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(username='$ARA_API_USERNAME').count()>0)" | ara-manage shell | tail -n 1)
 if [[ $result == "False" ]]; then
     echo "from django.contrib.auth.models import User; User.objects.create_superuser('$ARA_API_USERNAME', '$ARA_API_USERNAME@ara-server.local', '$ARA_API_PASSWORD')" | ara-manage shell
 fi
 
-exec gunicorn --workers $ARA_WORKERS --worker-class $ARA_WORKER_CLASS --bind $ARA_HOST:$ARA_PORT ara.server.wsgi
+exec unitd \
+  --no-daemon \
+  --control unix:/var/run/control.unit.sock \
+  --log /dev/stdout \
+  --user unit \
+  --group root
