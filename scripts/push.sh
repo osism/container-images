6f5b7663018f77f1edd098cb84ff6995716c2dd4
Christian Berendt <berendt@osism.tech>
2022-03-17 18:35:44 +0100

    Push some images with a specific version
    push e.g. osism/openstackclient:5.5.0
    push e.g. osism/ceph-daemon:12.2.13
    push e.g. osism/cephclient:16.2.5
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/push.sh
+++ scripts/push.sh
@@ -16,5 +16,41 @@ if [[ -n $DOCKER_REGISTRY ]]; then
 fi
 
 docker tag "$REPOSITORY:$REVISION" "$REPOSITORY:$VERSION"
+
+# push e.g. osism/cephclient:pacific
 docker push "$REPOSITORY:$VERSION"
+
+# push e.g. osism/openstackclient:5.5.0
+if [[ $IMAGE == "openstackclient" ]]; then
+    version=$(docker run --rm "$REPOSITORY:$VERSION" openstack --version | awk '{ print $2 }')
+    if skopeo inspect --creds "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" "docker://${REPOSITORY}:${version}" > /dev/null; then
+        echo "The image ${REPOSITORY}:${version} already exists."
+    else
+        docker tag "$REPOSITORY:$REVISION" "$REPOSITORY:$version"
+        docker push "$REPOSITORY:$version"
+    fi
+fi
+
+# push e.g. osism/ceph-daemon:12.2.13
+if [[ $IMAGE == "ceph-daemon" ]]; then
+    version=$(docker run --rm --entrypoint=/usr/bin/ceph "$REPOSITORY:$VERSION" --version | awk '{ print $3 }')
+    if skopeo inspect --creds "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" "docker://${REPOSITORY}:${version}" > /dev/null; then
+        echo "The image ${REPOSITORY}:${version} already exists."
+    else
+        docker tag "$REPOSITORY:$REVISION" "$REPOSITORY:$version"
+        docker push "$REPOSITORY:$version"
+    fi
+fi
+
+# push e.g. osism/cephclient:16.2.5
+if [[ $IMAGE == "cephclient" ]]; then
+    version=$(docker run --rm "$REPOSITORY:$VERSION" ceph --version | awk '{ print $3 }')
+    if skopeo inspect --creds "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" "docker://${REPOSITORY}:${version}" > /dev/null; then
+        echo "The image ${REPOSITORY}:${version} already exists."
+    else
+        docker tag "$REPOSITORY:$REVISION" "$REPOSITORY:$version"
+        docker push "$REPOSITORY:$version"
+    fi
+fi
+
 docker rmi "$REPOSITORY:$VERSION"
