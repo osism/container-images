84c722bd26ffbf27661b59145ed133e06749cd98
Christian Berendt <berendt@osism.tech>
2022-04-04 19:38:28 +0200

    Add seed image
    The seed image can be used as an alternative to the run.sh script
    in environments/manager to initially prepare a manager.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ seed/files/run.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+if [[ $# -lt 1 ]]; then
+    echo "usage: $0 PLAYBOOK [ANSIBLEARGS...]"
+    exit 1
+fi
+
+playbook=$1
+shift
+
+ANSIBLE_USER=${ANSIBLE_USER:-dragon}
+
+if [[ ! -e id_rsa.operator ]]; then
+
+    ansible-playbook \
+        -i localhost, \
+        -e @../secrets.yml \
+        osism.manager.keypair "$@"
+fi
+
+if [[ $playbook == "netbox" || $playbook == "traefik" ]]; then
+
+    ansible-playbook \
+        --private-key id_rsa.operator \
+        -i hosts \
+        -e @../images.yml \
+        -e @../configuration.yml \
+        -e @../secrets.yml \
+        -e @../infrastructure/images.yml \
+        -e @../infrastructure/configuration.yml \
+        -e @../infrastructure/secrets.yml \
+        -e @images.yml \
+        -e @configuration.yml \
+        -e @secrets.yml \
+        -u "$ANSIBLE_USER" \
+        osism.manager."$playbook" "$@"
+
+else
+
+    ansible-playbook \
+        --private-key id_rsa.operator \
+        -i hosts \
+        -e @../images.yml \
+        -e @../configuration.yml \
+        -e @../secrets.yml \
+        -e @images.yml \
+        -e @configuration.yml \
+        -e @secrets.yml \
+        -u "$ANSIBLE_USER" \
+        osism.manager."$playbook" "$@"
+
+fi
