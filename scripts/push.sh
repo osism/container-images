bb4bd94c72da40bed639d29746b0dc916e2656dd
Christian Berendt <berendt@osism.tech>
2022-11-03 20:57:32 +0100

    ara-server: only build the latest stable version (#187)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/push.sh
+++ scripts/push.sh
@@ -30,6 +30,17 @@ if [[ $IMAGE == "cgit" ]]; then
     fi
 fi
 
+# push e.g. osism/ara-server:1.5.8
+if [[ $IMAGE == "ara-server" ]]; then
+    version=$(docker run --rm "$REPOSITORY:$VERSION" ara --version | awk '{ print $2 }')
+    if skopeo inspect --creds "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" "docker://${REPOSITORY}:${version}" > /dev/null; then
+        echo "The image ${REPOSITORY}:${version} already exists."
+    else
+        docker tag "$REPOSITORY:$REVISION" "$REPOSITORY:$version"
+        docker push "$REPOSITORY:$version"
+    fi
+fi
+
 # push e.g. osism/openstackclient:5.5.0
 if [[ $IMAGE == "openstackclient" ]]; then
     version=$(docker run --rm "$REPOSITORY:$VERSION" openstack --version | awk '{ print $2 }')
