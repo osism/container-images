2de91a81904db88afd31ba91f7e130704fd774cf
Christian Berendt <berendt@osism.tech>
2023-01-12 18:35:22 +0100

    netbox: fix "Plugin netbox_bgp requires NetBox maximum version 3.3.99." (#216)
    This is a temporary workaround to be able to work with Netbox 3.4.
    
    Can be removed after merge & release of
    https://github.com/k01ek/netbox-bgp/pull/117
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ netbox/files/netbox-bgp-init.py
@@ -0,0 +1,21 @@
+from extras.plugins import PluginConfig
+from .version import __version__
+
+
+class BGPConfig(PluginConfig):
+    name = 'netbox_bgp'
+    verbose_name = 'BGP'
+    description = 'Subsystem for tracking bgp related objects'
+    version = __version__
+    author = 'Nikolay Yuzefovich'
+    author_email = 'mgk.kolek@gmail.com'
+    base_url = 'bgp'
+    required_settings = []
+    min_version = '3.2.0'
+    max_version = '3.4.99'
+    default_settings = {
+        'device_ext_page': 'right',
+    }
+
+
+config = BGPConfig # noqa
