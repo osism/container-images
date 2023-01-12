23c49fdd2f0b5f99270eaf8ff5e6f6a18a72d984
Christian Berendt <berendt@osism.tech>
2023-01-12 19:20:32 +0100

    netbox: add launch.sh script (#217)
    In this script, commands can be added that are to be executed
    after the initialisation of Netbox and before the start of
    Netbox.
    
    The initialisation of additional objects has been outsourced to
    a plug-in and must still be executed separately.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ netbox/files/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# In this script, commands can be added that are to be executed
+# after the initialisation of Netbox and before the start of
+# Netbox.
+
+# The initialisation of additional objects has been outsourced to
+# a plug-in and must still be executed separately.
+python3 /opt/netbox/netbox/manage.py load_initializer_data --path /opt/netbox/initializers
+
+exec /opt/netbox/launch-netbox.sh
