307b8bbbbbd697c99d7dcd0972aea0c883c486ee
Christian Berendt <berendt@osism.tech>
2024-09-20 14:37:21 +0200

    Add ansible-lint container image
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ ansible-lint/files/prepare-config.py
@@ -0,0 +1,12 @@
+import hiyapyco
+
+conf = hiyapyco.load(
+    "/zuul/.ansible-lint",
+    "/ansible-lint.yml",
+    method=hiyapyco.METHOD_MERGE,
+    interpolate=True,
+    failonmissingfiles=True,
+)
+
+with open("/zuul/.ansible-lint", "w+") as fp:
+    fp.write(hiyapyco.dump(conf, default_flow_style=True))
