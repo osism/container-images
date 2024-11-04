c23a0c4dbebc9cff73fb7e2efc66146295ac7891
Christian Berendt <berendt@osism.tech>
2024-11-04 18:24:50 +0100

    sonic-vs: tag with revision instead of version
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/build.sh
+++ scripts/build.sh
@@ -26,7 +26,7 @@ if [[ $IMAGE == "sonic-vs" ]]; then
     # URL from https://sonic.software/
     wget -q -O docker-sonic-vs.gz "https://artprodcus3.artifacts.visualstudio.com/Af91412a5-a906-4990-9d7c-f697b81fc04d/be1b070f-be15-4154-aade-b1d3bfb17054/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL21zc29uaWMvcHJvamVjdElkL2JlMWIwNzBmLWJlMTUtNDE1NC1hYWRlLWIxZDNiZmIxNzA1NC9idWlsZElkLzY4NTMzNi9hcnRpZmFjdE5hbWUvc29uaWMtYnVpbGRpbWFnZS52cw2/content?format=file&subpath=/target/docker-sonic-vs.gz"
     docker load --input docker-sonic-vs.gz
-    docker tag docker-sonic-vs:latest $DOCKER_REGISTRY/$REPOSITORY:$VERSION
+    docker tag docker-sonic-vs:latest $DOCKER_REGISTRY/$REPOSITORY:$REVISION
 elif [[ $IMAGE == "cephclient" ]]; then
     if [[ $VERSION == "quincy" ]]; then
         DEBIAN_VERSION=bullseye
