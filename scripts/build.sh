63b5ed85584411fcb599b50d205d2a2a80ddf003
Christian Berendt <berendt@osism.tech>
2025-01-04 21:04:02 +0100

    Fix sonic-vs build
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,9 +25,11 @@ cd $IMAGE
 
 if [[ $IMAGE == "sonic-vs" ]]; then
     # URL from https://sonic.software/
-    wget -q -O docker-sonic-vs.gz "https://artprodcus3.artifacts.visualstudio.com/Af91412a5-a906-4990-9d7c-f697b81fc04d/be1b070f-be15-4154-aade-b1d3bfb17054/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL21zc29uaWMvcHJvamVjdElkL2JlMWIwNzBmLWJlMTUtNDE1NC1hYWRlLWIxZDNiZmIxNzA1NC9idWlsZElkLzY4NTMzNi9hcnRpZmFjdE5hbWUvc29uaWMtYnVpbGRpbWFnZS52cw2/content?format=file&subpath=/target/docker-sonic-vs.gz"
+    if [[ "$VERSION" == "202411" ]]; then
+        wget -q -O docker-sonic-vs.gz "https://artprodcus3.artifacts.visualstudio.com/Af91412a5-a906-4990-9d7c-f697b81fc04d/be1b070f-be15-4154-aade-b1d3bfb17054/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL21zc29uaWMvcHJvamVjdElkL2JlMWIwNzBmLWJlMTUtNDE1NC1hYWRlLWIxZDNiZmIxNzA1NC9idWlsZElkLzczNzkwNi9hcnRpZmFjdE5hbWUvc29uaWMtYnVpbGRpbWFnZS52cw2/content?format=file&subpath=/target/docker-sonic-vs.gz"
+    fi
     docker load --input docker-sonic-vs.gz
-    docker tag docker-sonic-vs:latest $REPOSITORY:$REVISION
+    docker tag docker-sonic-vs:latest $REPOSITORY:$VERSION
     exit 0
 fi
 
