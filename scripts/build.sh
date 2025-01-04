ad2e1575769c253fa586f57fb32c01190e709e01
Christian Berendt <berendt@osism.tech>
2025-01-04 16:08:56 +0100

    openstackclient: use python 3.13 (#630)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,36 +21,37 @@ if [[ -n $DOCKER_REGISTRY ]]; then
     REPOSITORY="$DOCKER_REGISTRY/$REPOSITORY"
 fi
 
-pushd $IMAGE
+cd $IMAGE
+
 if [[ $IMAGE == "sonic-vs" ]]; then
     # URL from https://sonic.software/
     wget -q -O docker-sonic-vs.gz "https://artprodcus3.artifacts.visualstudio.com/Af91412a5-a906-4990-9d7c-f697b81fc04d/be1b070f-be15-4154-aade-b1d3bfb17054/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL21zc29uaWMvcHJvamVjdElkL2JlMWIwNzBmLWJlMTUtNDE1NC1hYWRlLWIxZDNiZmIxNzA1NC9idWlsZElkLzY4NTMzNi9hcnRpZmFjdE5hbWUvc29uaWMtYnVpbGRpbWFnZS52cw2/content?format=file&subpath=/target/docker-sonic-vs.gz"
     docker load --input docker-sonic-vs.gz
     docker tag docker-sonic-vs:latest $REPOSITORY:$REVISION
-elif [[ $IMAGE == "cephclient" ]]; then
+    exit 0
+fi
+
+if [[ $IMAGE == "cephclient" ]]; then
     if [[ $VERSION == "quincy" ]]; then
         DEBIAN_VERSION=bullseye
     else
 	DEBIAN_VERSION=bookworm
     fi
-
-    docker buildx build \
-      --load \
-      --build-arg "VERSION=$VERSION" \
-      --build-arg "DEBIAN_VERSION=$DEBIAN_VERSION" \
-      --tag "$REPOSITORY:$REVISION" \
-      --label "org.opencontainers.image.created=$CREATED" \
-      --label "org.opencontainers.image.revision=$REVISION" \
-      --label "org.opencontainers.image.version=$VERSION" \
-      $BUILD_OPTS .
-else
-    docker buildx build \
-      --load \
-      --build-arg "VERSION=$VERSION" \
-      --tag "$REPOSITORY:$REVISION" \
-      --label "org.opencontainers.image.created=$CREATED" \
-      --label "org.opencontainers.image.revision=$REVISION" \
-      --label "org.opencontainers.image.version=$VERSION" \
-      $BUILD_OPTS .
+    BUILD_OPTS="$BUILD_OPTS --build-arg DEBIAN_VERSION=$DEBIAN_VERSION"
+elif [[ $IMAGE == "openstackclient" ]]; then
+    if [[ $VERSION == "caracal" ]]; then
+        PYTHON_VERSION=3.12
+    else
+	PYTHON_VERSION=3.13
+    fi
+    BUILD_OPTS="$BUILD_OPTS --build-arg PYTHON_VERSION=$PYTHON_VERSION"
 fi
-popd
+
+docker buildx build \
+  --load \
+  --build-arg "VERSION=$VERSION" \
+  --tag "$REPOSITORY:$REVISION" \
+  --label "org.opencontainers.image.created=$CREATED" \
+  --label "org.opencontainers.image.revision=$REVISION" \
+  --label "org.opencontainers.image.version=$VERSION" \
+  $BUILD_OPTS .
